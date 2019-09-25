eab2922bdf000ce1f7e25c95c50bfd389752f0be
shivanimangla_successive <vaishalij@successive.com>
2019-09-25 18:02:55 +0530

    java_day16

---
+++ StringExample.java
@@ -0,0 +1,12 @@
+public class StringExample {
+    public static void main(String[] args) {
+        String s="vaishali";
+        int result = s.codePointAt(0);
+        System.out.println(result);
+
+        int result1=s.codePointBefore(2);
+        System.out.println(result1);
+
+
+    }
+}
